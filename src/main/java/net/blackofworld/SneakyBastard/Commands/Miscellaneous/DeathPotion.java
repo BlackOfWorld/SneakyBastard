7e26c66b63f5a946a6a221ed27476a96d190ec4f
BlackOfWorld <admin@blackofworld.fun>
2023-04-04 01:08:40 +0200

    feat(DeathPotion): added

---
+++ src/main/java/net/blackofworld/SneakyBastard/Commands/Miscellaneous/DeathPotion.java
@@ -0,0 +1,34 @@
+package net.blackofworld.SneakyBastard.Commands.Miscellaneous;
+
+import lombok.experimental.ExtensionMethod;
+import net.blackofworld.SneakyBastard.Command.CommandBase;
+import net.blackofworld.SneakyBastard.Command.CommandCategory;
+import net.blackofworld.SneakyBastard.Command.CommandInfo;
+import net.blackofworld.SneakyBastard.Extensions.PlayerExt;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.ArrayList;
+
+@CommandInfo(command = "deathpotion", category = CommandCategory.Miscellaneous, description = "Kill people in survival and in creative too!", Syntax = "")
+@ExtensionMethod({Player.class, PlayerExt.class})
+public class DeathPotion extends CommandBase
+{
+    @Override
+    public void Execute(Player p, ArrayList<String> args) {
+        ItemStack potion = new ItemStack(Material.SPLASH_POTION, 1);
+        PotionMeta potionmeta = (PotionMeta) potion.getItemMeta();
+        PotionEffect heal = new PotionEffect(PotionEffectType.HEAL, 2000, 125);
+        PotionEffect harm = new PotionEffect(PotionEffectType.HARM, 2000, 125);
+        potionmeta.addCustomEffect(heal, true);
+        potionmeta.addCustomEffect(harm, true);
+        potionmeta.setDisplayName("§6Splash Potion of §4§lDEATH");
+        potion.setItemMeta(potionmeta);
+        p.getInventory().addItem(potion);
+        p.getInventory().setItemInMainHand(potion);
+    }
+}
